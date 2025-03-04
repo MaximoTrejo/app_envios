d6134bd341d340a7a40396cc156844329f6ff577
MaximoTrejo <maximotrejo123@gmail.com>
2025-03-03 21:18:41 -0300

    cssPrueba1

--- web/js/main.js
+++ web/js/main.js
@@ -1,25 +1,24 @@
-import { obtenerToken, obtenerNuevoToken } from './auth.js';  // Asegúrate de importar las funciones necesarias
-import { obtenerOrdenPorId} from './api.js';  // Usar la función que maneja el token automáticamente
-import UI from './ui.js';  // Importar la clase UI
+import { obtenerToken, obtenerNuevoToken } from './auth.js'; 
+import { obtenerOrdenPorId} from './api.js'; 
+import UI from './ui.js';  
 
 document.addEventListener("DOMContentLoaded", async function () {
-    // Intentamos obtener el token desde localStorage
+
     let accessToken = await obtenerToken();
 
-    // Si no hay token, lo obtenemos y lo guardamos
     if (!accessToken) {
         console.log("Token no encontrado, obteniendo uno nuevo...");
 
-        // Obtener el código de autorización desde la URL
+
         const urlParams = new URLSearchParams(window.location.search);
-        const code = urlParams.get('code');  // Extraer el parámetro 'code' de la URL
+        const code = urlParams.get('code');  
 
         if (!code) {
             alert("Se necesita un código de autorización para obtener un nuevo token.");
             return;
         }
 
-        accessToken = await obtenerNuevoToken(code);  // Pasar el código de autorización a la función
+        accessToken = await obtenerNuevoToken(code);  
         localStorage.setItem('access_token', accessToken);
     }
 
@@ -47,7 +46,7 @@ document.addEventListener("DOMContentLoaded", async function () {
             console.log("Datos del envío:", envio);
 
             if (envio) {
-                UI.mostrarOrdenEnTabla([envio]);  // Usar el método estático de la clase UI
+                UI.mostrarOrdenEnTabla([envio]);
             } else {
                 alert("No se puede mostrar el envío.");
             }
