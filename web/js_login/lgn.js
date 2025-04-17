bf3452c0e89f49ea9802ff228c6f43e54072461f
MaximoTrejo <maximotrejo123@gmail.com>
2025-04-17 18:49:20 -0300

    prueba93

--- web/js_login/lgn.js
+++ web/js_login/lgn.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../redireccionamiento.env' });
 
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.getElementById("loginForm");
